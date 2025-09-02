45affe1d47c6281e72d328de5e8ccf8c212203b9
Nicolas Roche <roche@adacore.com>
2025-09-02 13:29:34 +0200

    Add github logging

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x
 set -e
+
+if [ "$GITHUB_STEP_SUMMARY" != "" ]; then
+   enable_github_reporting="true"
+else
+   enable_github_reporting="false"
+fi
+
 # Put gnat and gprbuild in the PATH
 export PATH=`pwd`/.build/installs/bin:$PATH
 
@@ -10,6 +17,8 @@ cd test
 python3 -m venv venv
 export PATH=`pwd`/venv/bin:$PATH
 pip install e3-testsuite
-../../nstd.gpr.py build --install --prefix=`pwd`/install --finalization=relaxed
+../../nstd.gpr.py build --install --prefix=`pwd`/install --finalization=relaxed 2>&1 | \
+   sed -e "s/^\(..*\):\([0-9]*\):\([0-9]*\): *warning: *\(.*\)/::warning file=\1,line=\2::\4/g"
+
 export GPR_PROJECT_PATH=`pwd`/install/share/gpr:$GPR_PROJECT_PATH
 ../../testsuite/run-tests -o ./out -d ./tmp
