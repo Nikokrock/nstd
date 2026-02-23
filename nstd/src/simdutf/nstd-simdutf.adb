ec6b3987fd4e5b76cab81239cf9a9ea3ba232d6a
Nicolas Roche <roche@adacore.com>
2026-02-23 14:04:00 +0100

    Add thin binding to SimdUTF
    Kind: feature

---
+++ nstd/src/simdutf/nstd-simdutf.adb
@@ -0,0 +1,48 @@
+--  Copyright (C) 2026, Nicolas Roche
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-Exception
+
+with Interfaces.C;
+
+package body NStd.Simdutf is
+
+   function Validate_UTF8 (Buf : Address; Len : SizeType) return Boolean
+   is
+      function Internal
+        (Buf : Address; Len : SizeType) return Interfaces.C.C_bool
+      with Import => True,
+        Convention => CPP,
+        External_Name => "_ZN7simdutf13validate_utf8EPKcm";
+   begin
+      return Boolean (Internal (Buf, Len));
+   end Validate_UTF8;
+
+   function Validate_ASCII (Buf : Address; Len : SizeType) return Boolean
+   is
+      function Internal
+        (Buf : Address; Len : SizeType) return Interfaces.C.C_bool
+      with Import => True,
+        Convention => CPP,
+        External_Name => "_ZN7simdutf14validate_asciiEPKcm";
+   begin
+      return Boolean (Internal (Buf, Len));
+   end Validate_ASCII;
+
+   function Find
+     (Start_Addr : Address; End_Addr : Address; B : Byte)
+     return Address
+   is
+      function Internal
+         (Start_Addr : Address;
+          End_Addr   : Address;
+          B             : Byte)
+         return Address
+      with Import        => True,
+           Convention    => CPP,
+           External_Name => "_ZN7simdutf4findEPKcS1_c";
+
+   begin
+      return Internal (Start_Addr, End_Addr, B);
+   end Find;
+
+end NStd.Simdutf;
---
+++ nstd/src/simdutf/nstd-simdutf.ads
@@ -0,0 +1,22 @@
+--  Copyright (C) 2026, Nicolas Roche
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-Exception
+--
+--  Provides a thin binding to SimdUTF C++ library. For documentation
+--  check the Simdutf project. Ada function names map to the C/C++ name
+
+with System; use System;
+
+package NStd.Simdutf is
+
+   function Validate_UTF8 (Buf : Address; Len : SizeType) return Boolean
+   with Inline_Always => True;
+
+   function Validate_ASCII (Buf : Address; Len : SizeType) return Boolean
+   with Inline_Always => True;
+
+   function Find
+     (Start_Addr : Address; End_Addr : Address; B : Byte) return Address
+   with Inline_Always => True;
+
+end NStd.Simdutf;
